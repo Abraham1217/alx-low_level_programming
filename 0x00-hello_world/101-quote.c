675a3874486297d10e5d417a79fb6cf810b49ff5
abraham <wole1702@gmial.com>
2023-03-02 03:24:00 -0800

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,8 @@
 #include <unistd.h>
-
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
 int main(void) 
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
